e5decb557cd491a019f05ae7a662f4606ab16931
Douglas Adolph <116844282+dcadolph@users.noreply.github.com>
2026-08-31 13:46:18 -0500

    Update loomseal cask to 1.0.0

--- Casks/loomseal.rb
+++ Casks/loomseal.rb
@@ -1,24 +1,24 @@
 cask "loomseal" do
-  version "0.5.0"
+  version "1.0.0"
 
   on_macos do
     on_intel do
-      sha256 "6b074b23e2b64616c89fc32dc3d971992f61acf62b05aa439e7255a363b2fec5"
+      sha256 "49ad10a40e84503cd6e4c0aa10d18a8517bff887657c234f80c94923a070ced1"
       url "https://github.com/kordloom/loomseal/releases/download/v#{version}/loomseal_#{version}_darwin_amd64.tar.gz"
     end
     on_arm do
-      sha256 "3c64294ba427d70946bb60388408cc99a924be6b3c0e2fce7ef11021369728b9"
+      sha256 "d8ef09f48c8fa69e8541e9055efc5839578f5e823b6fcba12809022a1309a25c"
       url "https://github.com/kordloom/loomseal/releases/download/v#{version}/loomseal_#{version}_darwin_arm64.tar.gz"
     end
   end
 
   on_linux do
     on_intel do
-      sha256 "e9e7d4c22fcd5bd216292158ad2946a995080d198e0a5d76b945a6f6af48b1ca"
+      sha256 "40fdb61493bb84483ab8d939e1c5ebc2021363567ee1b0ff79a69fe1ec5db248"
       url "https://github.com/kordloom/loomseal/releases/download/v#{version}/loomseal_#{version}_linux_amd64.tar.gz"
     end
     on_arm do
-      sha256 "db76282b011f473de575557deb1ddb6af5188dcbe953b7d98f8e3cdc35ddd6bb"
+      sha256 "54a32b31ab9d2eb2bb8925fa6fe2afe1b8ff35fdf2225f68abd88f58b7fc436a"
       url "https://github.com/kordloom/loomseal/releases/download/v#{version}/loomseal_#{version}_linux_arm64.tar.gz"
     end
   end
